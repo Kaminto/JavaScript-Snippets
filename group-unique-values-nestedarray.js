2324b842134eca9eb94be80cc23513be3d714750
Kaminto <kaminto@outlook.com>
2021-08-31 22:30:34 +0300

    1 file modified

--- group-unique-values-nestedarray.js
+++ group-unique-values-nestedarray.js
@@ -1,12 +1,18 @@
 let onr = [
-  ['fvw',189,'m^2', 0,0,0, 'cement-200', 'water-600', 'sand-4500' ],
-  ['fvs',18,'m^3', 0,0,0, 'cement-400', 'water-400', 'sand-500' ],
-  ['fvw',189,'m^4', 0,0,0, 'cement-300', 'water-90', 'sand-400' ],
+  ['fvw',189,'m^2', 0,0,0, 'cement-200', 'water-600', 'sand-4500', null ,null,null ],
+  ['fvs',18,'m^3', 0,0,0, 'cement-400', 'water-400', 'sand-500', null ,null,null ],
+  ['fvw',189,'m^4', 0,0,0, 'cement-300', 'water-90', 'sand-400', null ,null,null ],
 ]
+let jnm = onr.map((r,i)=>{
+  return [r[6],r[7],r[8],r[9],r[10],r[11]]
+});
 
-console.log('jnm',onr.map((r,i)=>{
-  return [r[6],r[7],r[8]]
-}))
+let flatten2 = [].concat(...jnm).filter(e=>{   
+  return e;
+}).sort();
+console.log('flatten2', flatten2);
+// 11 - 16
+console.log('jnm',jnm)
 
 
 let on = [
@@ -30,10 +36,25 @@ const groupUnique = arr => {
       console.log('dl', item);
       map[item.split('-')[0]] = [];
       temp[item.split('-')[0]] = map[item.split('-')[0]];
-      res.push(temp);
+      res.push(temp); 
     }
-    map[item.split('-')[0]].push(item);
+    map[item.split('-')[0]].push(Number(item.split('-')[1]));
   });
   return res;
 };
-console.log(groupUnique(flatten));
+
+let grouped = groupUnique(flatten)
+
+
+console.log('grouped2', grouped.map(e=>{
+  console.log(e,Object.keys(e)[0],e[Object.keys(e)[0]].reduce((a, b) => a + b))
+  let gk = {};
+  gk[Object.keys(e)[0]] = e[Object.keys(e)[0]].reduce((a, b) => a + b)
+  console.log(gk)
+  return gk
+}));
+ 
+
+console.log('grouped', grouped.map(e=>{
+  return [Object.keys(e)[0],e[Object.keys(e)[0]].reduce((a, b) => a + b)]
+}));
